95e006adc05d04f7649b74bac9b839f480977bcc
Mauro Carlos <mauro_carlos14@hotmail.com>
2025-09-16 08:02:20 -0400

    feito oa config repo remoto

---
+++ src/escola/SistemaCadastro.java
@@ -0,0 +1,20 @@
+package escola;
+
+public class SistemaCadastro {
+	
+	public static void main(String[] args) {
+		
+		Pessoa marcos = new Pessoa("FELIPE" , "222222-22");
+		
+		Pessoa junior = new Pessoa("Junior");
+		
+		
+		marcos.setEndereco("RUA DAS MARIAS");
+		
+		
+		System.out.println(marcos.getNome() + "-" + marcos.getCpf());
+		System.out.println(junior.getNome());
+		
+	}
+
+}
