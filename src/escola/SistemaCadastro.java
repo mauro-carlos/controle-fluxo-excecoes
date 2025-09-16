95f1ef614f875b7b29b3c0e197b3248f87b5759c
Mauro Carlos <mauro_carlos14@hotmail.com>
2025-09-16 08:15:08 -0400

    commit projeto construtores

--- src/escola/SistemaCadastro.java
+++ src/escola/SistemaCadastro.java
@@ -4,6 +4,9 @@ public class SistemaCadastro {
 	
 	public static void main(String[] args) {
 		
+		
+		
+		
 		Pessoa marcos = new Pessoa("FELIPE" , "222222-22");
 		
 		Pessoa junior = new Pessoa("Junior");
